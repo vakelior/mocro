7429c1770d9177e3f6c219cd36e90278cbf2247a
vakelior <vakelior@gmail.com>
2026-09-17 20:20:49 +0100

    Revert author avatar zoom

--- assets/js/article.js
+++ assets/js/article.js
@@ -205,9 +205,7 @@
       avatar = el('span', 'author-card-avatar placeholder');
       avatar.appendChild(icon('person'));
     }
-    var avWrap = el('div', 'author-card-avatar-wrap');
-    avWrap.appendChild(avatar);
-    inner.appendChild(avWrap);
+    inner.appendChild(avatar);
 
     var body = el('div', 'author-card-body');
     body.appendChild(el('div', 'author-card-eyebrow', 'الكاتب'));
